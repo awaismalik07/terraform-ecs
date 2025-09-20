8f21d4c79af10860e4af77732e690c61e369b5b6
Awais Malik <muhamadawaismalik@gmail.com>
2025-09-20 05:19:16 +0500

    Added Public and Private Subnets

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,7 +1,43 @@
-resource "aws_vpc" "vpc" {
-    cidr_block = var.vpcCidr
+#VPC
+resource "aws_vpc" "Vpc" {
+    cidr_block  = var.vpcCidr
 
     tags = {
-        Name = "${var.owner}-${var.env}-Vpc"
+        Name    = "${var.owner}-${var.env}-Vpc"
     }   
 }
+
+#Get All Availability Zones
+data "aws_availability_zone" "AZs" {
+    state       = "available"
+    filter {
+      name      = "opt-in-status"
+      values    = ["opt-in-not-required"]
+    }
+}
+
+#Creates two public subnets in first two AZs
+resource "aws_subnet" "PublicSubnets" {
+    count                   = 2
+    vpc_id                  = aws_vpc.Vpc.id
+    availability_zone       = data.aws_availability_zone.AZs.names[count.index]
+    cidr_block              = cidrsubnet(aws_vpc.Vpc.cidr_block, 8, count.index)
+    map_public_ip_on_launch = true
+    tags = {
+      Name                  = "${var.owner}-${var.env}-PublicSubnet-${data.aws_availability_zone.AZs.names[count.index]}"
+    }
+}
+
+#Creates two private subnets in first two AZs
+resource "aws_subnet" "PublicSubnets" {
+    count                   = 2
+    vpc_id                  = aws_vpc.Vpc.id
+    availability_zone       = data.aws_availability_zone.AZs.names[count.index]
+    cidr_block              = cidrsubnet(aws_vpc.Vpc.cidr_block, 8, count.index + 2)
+    map_public_ip_on_launch = true
+    tags = {
+      Name                  = "${var.owner}-${var.env}-PrivateSubnet-${data.aws_availability_zone.AZs.names[count.index]}"
+    }
+}
+
+
